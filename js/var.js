6232bacbf8b1664d837ebfe9509a55c8ec1abfb1
MiguelPeralta123 <maikperalta123@gmail.com>
2023-01-11 13:05:39 -0700

    Added index.js and var.js

---
+++ js/var.js
@@ -0,0 +1,29 @@
+// Variables with var
+
+var product = 'Gamer headphones'
+
+var available
+
+product = true
+
+available = true
+
+var product1 = 'computer',
+    available1 = true,
+    category = 'computers'
+
+// NAME RULES
+
+// INVALID NAMES
+/*
+var 1product = 'computer'
+var -product = 'computer'
+var %product = 'computer'
+*/
+
+// VALID NAMES
+// JavaScript is case sensitive
+var product_name = 'computer' // underscore
+var productName = 'computer' // camelCase
+var ProductName = 'computer' // PascalCase (only for classes)
+var productname = 'computer' // lowercase
